5e063e588093d4016ccbf1fbd59dce14930e67d0
Ismael Vargas <ismael.ubk@gmail.com>
2024-06-19 17:30:26 -0500

    update base footer: Build with Observable

--- observablehq.config.js
+++ observablehq.config.js
@@ -27,7 +27,7 @@ export default {
   // Some additional configuration options and their defaults:
   // theme: "default", // try "light", "dark", "slate", etc.
   // header: "", // what to show in the header (HTML)
-  // footer: "Built with Observable.", // what to show in the footer (HTML)
+  footer: `Built with <a href="https://observablehq.com/">Observable</a>.`, // what to show in the footer (HTML)
   // sidebar: true, // whether to show the sidebar
   // toc: true, // whether to show the table of contents
   pager: false, // whether to show previous & next links in the footer
